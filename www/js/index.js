f069e6ea8ceeaade2e1f587b6764a3450ca6c7a5
DESKTOP-DQ3ODV6\MARIEYFRAN <falvarino@ximiodev.com>
2019-03-29 15:02:41 -0300

    con cilleaera

--- www/js/index.js
+++ www/js/index.js
@@ -36,7 +36,95 @@ var app = {
         console.log('Received Device Ready Event');
         console.log('calling setup push');
         app.setupPush();
+        app.initStore();
     },
+    initStore: function() {
+		if (!window.store) {
+			log('Store not available');
+			return;
+		}
+		
+		app.platform = device.platform.toLowerCase();
+		document.getElementsByTagName('body')[0].className = app.platform;
+
+		// Enable maximum logging level
+		store.verbosity = store.DEBUG;
+		store.register({
+			id:    'subscription1', // id without package name!
+			alias: 'subscription1',
+			type:  store.PAID_SUBSCRIPTION
+		});
+			
+		store.when("subscription1").approved(function(p) {
+			log("verify subscription");
+			p.verify();
+		});
+		store.when("subscription1").verified(function(p) {
+			log("subscription verified");
+			p.finish();
+		});
+		store.when("subscription1").unverified(function(p) {
+			log("subscription unverified");
+		});
+		store.when("subscription1").updated(function(p) {
+			if (p.owned) {
+				document.getElementById('subscriber-info').innerHTML = 'You are a lucky subscriber!';
+			}
+			else {
+				document.getElementById('subscriber-info').innerHTML = 'You are not subscribed';
+			}
+		});
+
+		// Log all errors
+		store.error(function(error) {
+			log('ERROR ' + error.code + ': ' + error.message);
+		});
+		
+		store.ready(function() {
+			var el = document.getElementById("loading-indicator");
+			if (el)
+				el.style.display = 'none';
+		});
+
+		// When store is ready, activate the "refresh" button;
+		store.ready(function() {
+			var el = document.getElementById('refresh-button');
+			if (el) {
+				el.style.display = 'block';
+				el.onclick = function(ev) {
+					store.refresh();
+				};
+			}
+		});
+	},
+	renderIAP: function(p) {
+
+		var parts = p.id.split(".");
+		var elId = parts[parts.length-1];
+
+		var el = document.getElementById(elId + '-purchase');
+		if (!el) return;
+
+		if (!p.loaded) {
+			el.innerHTML = '<h3>...</h3>';
+		}
+		else if (!p.valid) {
+			el.innerHTML = '<h3>' + p.alias + ' Invalid</h3>';
+		}
+		else if (p.valid) {
+			var html = "<h3>" + p.title + "</h3>" + "<p>" + p.description + "</p>";
+			if (p.canPurchase) {
+				html += "<div class='button' id='buy-" + p.id + "' productId='" + p.id + "' type='button'>" + p.price + "</div>";
+			}
+			el.innerHTML = html;
+			if (p.canPurchase) {
+				document.getElementById("buy-" + p.id).onclick = function (event) {
+					var pid = this.getAttribute("productId");
+					store.order(pid);
+				};
+			}
+		}
+	},
     setupPush: function() {
         console.log('calling push init');
         var push = PushNotification.init({
@@ -85,4 +173,23 @@ var app = {
             );
        });
     }
+};
+
+function log(arg) { app.log(arg); }
+
+// log both in the console and in the HTML #log element.
+app.log = function(arg) {
+    try {
+        if (typeof arg !== 'string')
+            arg = JSON.stringify(arg);
+        console.log(arg);
+        document.getElementById('log').innerHTML += '<div>' + arg + '</div>';
+    } catch (e) {}
+};
+
+// make sure fn will be called with app as 'this'
+app.bind = function(fn) {
+    return function() {
+        fn.call(app, arguments);
+    };
 };
